9bdfc06cdb9e7aa754e30f407327d7a6bb75fafe
Documenter.jl <documenter@juliadocs.github.io>
2022-12-16 13:09:40 +0000

    build based on 211a768

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BiochemicalVisualization","category":"page"},{"location":"#BiochemicalVisualization","page":"Home","title":"BiochemicalVisualization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BiochemicalVisualization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BiochemicalVisualization]","category":"page"}]
+}
