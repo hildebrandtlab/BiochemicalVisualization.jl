59acd91fe89bc1a3b92cb1a09537d58aa5cce137
Thomas Kemmer <thomas@binaryden.de>
2024-10-16 15:10:41 +0200

    Aqua: ignore remaining type piracy for now (#10)
    Signed-off-by: Thomas Kemmer <thomas@binaryden.de>

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,5 +1,9 @@
 @testitem "Aqua" begin
     using Aqua
+    using MsgPack
 
-    Aqua.test_all(BiochemicalVisualization)
+    Aqua.test_all(BiochemicalVisualization;
+        # workaround for https://github.com/hildebrandtlab/BiochemicalVisualization.jl/issues/10
+        piracies = (; treat_as_own = [MsgPack.msgpack_type])
+    )
 end
